5f5171415308dec290dd19dbfeaa9a962277375b
Enrique-N <enrique.xe@gmail.com>
2021-12-09 14:15:47 -0500

    update

--- EJS/index.js
+++ EJS/index.js
@@ -1,28 +1,40 @@
 let express = require('express');
 let app = express();
-let ejs = require('ejs')
+let { Server: SocketIO } = require("socket.io")
+let { Server: HttpServer } = require("http")
+const httpServer = new HttpServer(app);
+const io = new SocketIO(httpServer);
 let path = require('path')
 let Contenedor = require("./productos");
 let contenedor = new Contenedor("./productos.txt")
 const PORT = 3000
 
+let obj = []
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
-
 app.set("views", path.join(__dirname, "views", "ejs"));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res, next) => {
-    res.render("index", { data })
+    res.render("index", { obj: obj })
 })
 
 app.post("/productos", (req, res, next) => {
-    contenedor.save(req.body)
     res.redirect("/")
 })
 
+io.on('connection', socket => {
+    console.log(`Usuario conectado ${socket.id}`)
+    socket.on("infoProductos", item => {
+        obj = data
+        contenedor.save(item)
+        io.sockets.emit("mi_sala", data)
+    })
+    socket.emit("init", data)
+})
 
-app.listen(PORT, () => {
+
+httpServer.listen(PORT, () => {
     console.log(`http://localhost:${PORT}`)
 })
